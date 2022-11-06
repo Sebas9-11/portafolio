d7f6c715d030274fa25a6dbe0f083fd7d3e7bd7d
Sebas9-11 <sebastian-1999@outlook.com>
2022-11-05 22:30:00 -0500

    other commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,102 +1,3 @@
-// import React from "react";
-
-// export default function Contact() {
-//   return (
-//     <section id="contact" className="relative">
-//       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-//         <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-//           <iframe
-//             width="100%"
-//             height="100%"
-//             title="map"
-//             className="absolute inset-0"
-//             frameBorder={0}
-//             marginHeight={0}
-//             marginWidth={0}
-//             style={{ filter: "opacity(0.7)" }}
-//             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15666.746135064102!2d-74.97023860560553!3d10.987016399014031!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef42a97c92c0b29%3A0x3ce086a172da1be6!2sCra.%209%2C%20Puerto%20Colombia%2C%20Atl%C3%A1ntico!5e0!3m2!1ses!2sco!4v1667697582584!5m2!1ses!2sco"
-//           />
-//           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-//             <div className="lg:w-1/2 px-6">
-//               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-//                 DIRECCIÓN
-//               </h2>
-//               <p className="mt-1">Puerto Colombia, Atlántico, Colombia</p>
-//             </div>
-//             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-//               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-//                 CORREO ELECTRÓNICO
-//               </h2>
-//               <a className="text-indigo-450 leading-relaxed">
-//                 sebastian-1999@outlook.com
-//               </a>
-//               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
-//                 NÚMERO DE TELÉFONO
-//               </h2>
-//               <p className="leading-relaxed">+57 301-321-3527</p>
-//             </div>
-//           </div>
-//         </div>
-//         <form
-//           netlify
-//           name="contact"
-//           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
-//         >
-//           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-//             Contactame
-//           </h2>
-//           <p className="leading-relaxed mb-5">
-//             Para consultas, dudas o sugerencias, puedes contactarme a través de
-//             este formulario.
-//           </p>
-//           <div className="relative mb-4">
-//             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
-//               Nombre
-//             </label>
-//             <input
-//               type="text"
-//               id="name"
-//               name="name"
-//               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-//             />
-//           </div>
-//           <div className="relative mb-4">
-//             <label htmlFor="email" className="leading-7 text-sm text-gray-400">
-//               Correo electrónico
-//             </label>
-//             <input
-//               type="email"
-//               id="email"
-//               name="email"
-//               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-//             />
-//           </div>
-//           <div className="relative mb-4">
-//             <label
-//               htmlFor="message"
-//               className="leading-7 text-sm text-gray-400"
-//             >
-//               Mensaje
-//             </label>
-//             <textarea
-//               id="message"
-//               name="message"
-//               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-//             />
-//           </div>
-//           <button
-//             type="submit"
-//             className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-//           >
-//             Enviar
-//           </button>
-//         </form>
-//       </div>
-//     </section>
-//   );
-
-// }
-
 import React from "react";
 
 export default function Contact() {
@@ -143,22 +44,19 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 DIRECCIÓN
               </h2>
-              <p className="mt-1">
-                97 Warren St. <br />
-                New York, NY 10007
-              </p>
+              <p className="mt-1">Puerto Colombia, Atlántico, Colombia</p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 CORREO ELECTRÓNICO
               </h2>
               <a className="text-indigo-400 leading-relaxed">
-                reedbarger@email.com
+                sebastian-1999@outlook.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 NÚMERO DE TELÉFONO
               </h2>
-              <p className="leading-relaxed">123-456-7890</p>
+              <p className="leading-relaxed">+57 301-321-3527</p>
             </div>
           </div>
         </div>
@@ -169,11 +67,11 @@ export default function Contact() {
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            ¡Contrátame!
+            Contactame
           </h2>
           <p className="leading-relaxed mb-5">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-            suscipit officia aspernatur veritatis. Asperiores, aliquid?
+            Para consultas, dudas o sugerencias, puedes contactarme a través de
+            este formulario.
           </p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
@@ -215,7 +113,7 @@ export default function Contact() {
           </div>
           <button
             type="submit"
-            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+            className="text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
           >
             Enviar
           </button>
